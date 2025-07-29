4b1e86e7f31cbe4460d7fa697217a47024e1e9ba
KairiDaiho <kairidaiho12@gmail.com>
2025-07-29 23:55:00 +0900

    Add homepage and Router basename for GH Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <AuthProvider>
       <CommunityProvider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
